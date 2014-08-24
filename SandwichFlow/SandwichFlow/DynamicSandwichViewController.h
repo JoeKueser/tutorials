3eaa5dd02515942d6ea87bda213e9b548305ee3d
JoeKueser <joe.kueser@gmail.com>
2014-08-23 19:40:29 -0700

    Adding SandwichFlow portion of tutorial.

---
+++ SandwichFlow/SandwichFlow/DynamicSandwichViewController.h
@@ -0,0 +1,13 @@
+//
+//  DynamicSandwichViewController.h
+//  SandwichFlow
+//
+//  Created by Joe Kueser on 8/23/14.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DynamicSandwichViewController : UIViewController
+
+@end
